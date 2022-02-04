9698d84c0a771730ada90b01f7db0a5d95bc1c60
Erin Huang <erin.huang09@gmail.com>
2022-02-04 20:12:35 +0800

    Update 2022-02-04T12:12:31.292Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,33 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var url = 'https://vue3-course-api.hexschool.io/v2';
+var path = 'erinhuang-lab';
+"use strict";
+
+// 串接登入 API
+var loginApp = Vue.createApp({
+  data: function data() {
+    return {
+      user: {
+        username: '',
+        password: ''
+      }
+    };
+  },
+  methods: {
+    login: function login() {
+      axios.post("".concat(url, "/admin/signin"), this.user).then(function (res) {
+        // 將 token 存在 cookie
+        var _res$data = res.data,
+            token = _res$data.token,
+            expired = _res$data.expired;
+        document.cookie = "erinToken=".concat(token, "; expires=").concat(new Date(expired), "GMT;"); // 跳轉頁面
+
+        window.alert("".concat(res.data.message, "\uFF0C\u5373\u5C07\u9032\u5165\u5546\u54C1\u9801\u9762"));
+        window.location.href;
+      });
+    }
+  }
 });
+loginApp.mount('#loginApp');
 //# sourceMappingURL=all.js.map
